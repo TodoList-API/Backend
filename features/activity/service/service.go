080f4bd2e74db556bad52bdeec2e04cd086f09c4
Herdiladania <herdiladania11@gmail.com>
2023-03-24 11:02:00 +0700

    feat : add create activity service

--- features/activity/service/service.go
+++ features/activity/service/service.go
@@ -1,1 +1,39 @@
 package service
+
+import (
+	"TodoApp/features/activity"
+)
+
+type activityService struct {
+	qry activity.ActivityData
+}
+
+func New(qry activity.ActivityData) activity.ActivityService {
+	return &activity.ActivityService{
+		qry: ad,
+	}
+}
+
+func (as *activityService) Create(newActivity activity.Core) (activity.Core, error) {
+	res, err := as.qry.Create(newActivity)
+
+	if err != nil {
+		return activity.Core{}, err
+
+	}
+
+	return res, nil
+}
+
+func (as *activityService) Update(activityID uint, updatedActivity activity.Core) (activity.Core, error) {
+	return activity.Core{}, nil
+}
+func (as *activityService) ListActivity() ([]activity.Core, error) {
+	return []activity.Core{}, nil
+}
+func (as *activityService) GetActivity(activityID uint) (activity.Core, error) {
+	return activity.Core{}, nil
+}
+func (as *activityService) Delete(activityID uint) error {
+	return nil
+}
