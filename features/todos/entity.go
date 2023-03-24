0612164f4fc52b19b22aee28fa32f9cbe7a2e32b
Herdiladania <herdiladania11@gmail.com>
2023-03-24 15:53:03 +0700

    add todo entity

--- features/todos/entity.go
+++ features/todos/entity.go
@@ -1,1 +1,41 @@
-package entity
+package todos
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	ID              uint
+	Title           string
+	ActivityGroupId uint
+	IsActive        bool
+	Priority        string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type TodoHandler interface {
+	Create() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	ListTodo() echo.HandlerFunc
+	GetTodo() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+}
+
+type TodoService interface {
+	Create(newTodo Core) (Core, error)
+	Update(todoID uint, updatedTodo Core) (Core, error)
+	ListTodo() ([]Core, error)
+	GetTodo(todoID uint) (Core, error)
+	Delete(todoID uint) error
+}
+
+type TodoData interface {
+	Create(newTodo Core) (Core, error)
+	Update(todoID uint, updatedTodo Core) (Core, error)
+	ListTodo() ([]Core, error)
+	GetTodo(todoID uint) (Core, error)
+	Delete(todoID uint) error
+}
