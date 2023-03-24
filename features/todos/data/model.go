3f23d83c6e5e7fa85a81e89c1ebb966bdba1b837
Herdiladania <herdiladania11@gmail.com>
2023-03-24 16:18:34 +0700

    feat : add todo query

--- features/todos/data/model.go
+++ features/todos/data/model.go
@@ -1,1 +1,55 @@
 package data
+
+import (
+	"TodoApp/features/todos"
+
+	"gorm.io/gorm"
+)
+
+type Todos struct {
+	gorm.Model
+	Title           string
+	ActivityGroupId uint
+	IsActive        bool
+	Priority        string
+}
+
+func DataToCore(data Todos) todos.Core {
+	return todos.Core{
+		ID:              data.ID,
+		Title:           data.Title,
+		ActivityGroupId: data.ActivityGroupId,
+		IsActive:        data.IsActive,
+		Priority:        data.Priority,
+		CreatedAt:       data.CreatedAt,
+		UpdatedAt:       data.UpdatedAt,
+	}
+}
+
+func CoreToData(core todos.Core) Todos {
+	return Todos{
+		Model:           gorm.Model{ID: core.ID, CreatedAt: core.CreatedAt, UpdatedAt: core.UpdatedAt},
+		Title:           core.Title,
+		ActivityGroupId: core.ActivityGroupId,
+		IsActive:        core.IsActive,
+		Priority:        core.Priority,
+	}
+}
+
+func ListDataToCore(data []Todos) []todos.Core {
+	var listTodos []todos.Core
+	for _, todo := range data {
+		listTodos = append(listTodos, DataToCore(todo))
+	}
+
+	return listTodos
+}
+
+func ListCoreToData(data []todos.Core) []Todos {
+	var listTodos []Todos
+	for _, todo := range data {
+		listTodos = append(listTodos, CoreToData(todo))
+	}
+
+	return listTodos
+}
--- features/todos/data/query.go
+++ features/todos/data/query.go
@@ -1,1 +1,84 @@
 package data
+
+import (
+	"TodoApp/features/todos"
+	"errors"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type todoData struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) todos.TodoData {
+	return &todoData{
+		db: db,
+	}
+}
+
+func (td *todoData) Create(newTodo todos.Core) (todos.Core, error) {
+
+	data := CoreToData(newTodo)
+	err := td.db.Create(&data).Error
+
+	if err != nil {
+		return todos.Core{}, err
+	}
+
+	return DataToCore(data), nil
+}
+
+func (td *todoData) Update(todoID uint, updatedTodo todos.Core) (todos.Core, error) {
+	data := CoreToData(updatedTodo)
+
+	qry := td.db.Where("id = ?", todoID).Updates(&data).Find(&data)
+
+	if qry.RowsAffected <= 0 {
+		return todos.Core{}, errors.New("data not found")
+	}
+
+	if err := qry.Error; err != nil {
+		return todos.Core{}, err
+	}
+
+	return DataToCore(data), nil
+}
+func (td *todoData) ListTodo() ([]todos.Core, error) {
+	listTodo := []Todos{}
+
+	err := td.db.Find(&listTodo).Error
+
+	if err != nil {
+		return []todos.Core{}, err
+
+	}
+
+	return ListDataToCore(listTodo), nil
+}
+func (td *todoData) GetTodo(todoID uint) (todos.Core, error) {
+	myTodo := Todos{}
+
+	qry := td.db.Where("id = ?", todoID).Find(&myTodo)
+
+	if err := qry.Error; err != nil {
+		return todos.Core{}, err
+	} else if qry.RowsAffected <= 0 {
+		msg := fmt.Sprintf("Todo with ID %d Not Found", todoID)
+		return todos.Core{}, errors.New(msg)
+	}
+	return DataToCore(myTodo), nil
+}
+func (td *todoData) Delete(todoID uint) error {
+	record := Todos{}
+
+	err := td.db.Where("id = ?", todoID).Delete(&record).Error
+
+	if err != nil {
+		return err
+
+	}
+
+	return nil
+}
